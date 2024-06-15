24d2a8be06850521b3a869b3a8846f52f001ef4e
Dirk Troost <41777739+WorkshopDirk@users.noreply.github.com>
2024-06-15 15:32:10 +0200

    added authenticator

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+//
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
 </script>
 
 <template>
   <main>
+    <authenticator>
     <Todos />
+  </authenticator>
   </main>
 </template>
 
